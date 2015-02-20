6167c031df7d2c1cb277c75f0587aba020649093
craftpaperbag <kazu11518@gmail.com>
2015-02-21 02:21:41 +0900

    #1 haribote station name

--- sobu.js
+++ sobu.js
@@ -1,19 +1,16 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+  Template.station.helpers({
+    kanji: function () {
+      // TODO: use location information
+      return "本八幡"; 
+    },
+    hiragana: function () {
+      // TODO: use location information
+      return "もとやわた";
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
 }
 
 if (Meteor.isServer) {
