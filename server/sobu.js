4aa917dcf86e4179ddd577dec6b1db15bbf44e11
craftpaperbag <kazu11518@gmail.com>
2015-02-21 03:50:22 +0900

    #2 add stations if Stations: 0

--- server/sobu.js
+++ server/sobu.js
@@ -1,4 +1,35 @@
+//
+// ekimei preparation
+//
+
+//
+//
+// remove all stations.
+// only use for debug
+//
+// Stations.remove({});
 
 if ( ! Stations.findOne() ) {
   console.log("Stations length is 0.");
+  var txt = Assets.getText('ekimei.txt');
+  var lines = txt.split("\n");
+  for (var i in lines) {
+    var tabs = lines[i].split("\t");
+    if ( tabs.length !== 5 ) {
+      console.log("length: " + tabs.length + " skip.(" + lines[i] + ")");
+      continue;
+    }
+    var s = {
+      number: Number(tabs[0]),
+      name: tabs[1],
+      x: Number(tabs[2]),
+      y: Number(tabs[3]),
+      hiragana: tabs[4],
+      version: '0.0.1',
+    }
+    console.log(s);
+    Stations.insert(s);
+  }
+  var length = Stations.find().count();
+  console.log("Stations length now " + length);
 }
