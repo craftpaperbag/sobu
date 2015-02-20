7271d2c9fcfa257afaa15a76ee17e982667493cb
craftpaperbag <kazu11518@gmail.com>
2015-02-21 02:58:09 +0900

    add private/

--- private/ekiget.js
+++ private/ekiget.js
@@ -1,3 +1,18 @@
+console.log('get ekimei');
+
 var request = require('request');
 
+var qs = {
+  method: "getStations",
+  line: "総武線",
+};
 
+var opts = {
+  url: "http://express.heartrails.com/api/json",
+  qs: qs,
+};
+
+request(opts, function (err, response, body) {
+  var result = JSON.parse(body);
+  console.log(body);
+});
